2e6221f833757b535e0819d54be7ca98b11021db
vladimir-molotkov <molotkov1vl@gmail.com>
2025-06-17 11:16:23 +0300

    update bert

--- models/bert_model.py
+++ models/bert_model.py
@@ -1,2 +1,14 @@
+from datasets import load_dataset
+from transformers import BertForQuestionAnswering, BertTokenizer
+
+# import evaluate
+# import numpy as np
 # import torch
-# from transformers import BertForQuestionAnswering, BertTokenizer
+
+# Load model and tokenizer
+model_name = "bert-base-uncased"
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = BertForQuestionAnswering.from_pretrained(model_name)
+
+dataset = load_dataset("squad")
+print("Dataset loaded:", dataset)
